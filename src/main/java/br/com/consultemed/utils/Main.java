730bbf38f8ebd88e7e3721630ab0c16223ee5856
Emiliano Carvalho <emilianocarvalho.dev@gmail.com>
2019-06-29 02:05:13 -0300

    Edit main

--- src/main/java/br/com/consultemed/utils/Main.java
+++ src/main/java/br/com/consultemed/utils/Main.java
@@ -11,21 +11,21 @@ import br.com.consultemed.model.Contato;
 public class Main {
 
 	public static void main(String[] args) {
-		EntityManager entityManager  = JPAUtils.getEntityManager();
+		EntityManager entityManager = JPAUtils.getEntityManager();
 
 		System.out.println("Criando o contato");
 		Contato contato = new Contato();
-		
+
 		contato.setNome("Emiliano Fernandes");
 		contato.setEmail("emilianocarvalho.dev@gmail.com");
-		  		
+
 //        Set<String> telefones = Stream.of("83 99692-3678", "83 99692-3778", "83 99692-3878")
 //        		  .collect(Collectors.toCollection(HashSet::new));
 
-        Set<String> telefones = new HashSet<>(Arrays.asList("83 99692-3678", "83 99692-3778", "83 99692-3878"));
-        
+		Set<String> telefones = new HashSet<>(Arrays.asList("83 99692-3678", "83 99692-3778", "83 99692-3878"));
+
 		contato.setTelefones(telefones);
-		
+
 		entityManager.getTransaction().begin();
 		entityManager.persist(contato);
 		entityManager.getTransaction().commit();
